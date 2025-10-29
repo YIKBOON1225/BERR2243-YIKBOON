9b74cca17c6bd8fdf5d1d78562a21b814c921500
LIM YIK BOON <b122420030@student.utem.edu.my>
2025-10-29 06:54:13 +0000

    before question

--- index.js
+++ index.js
@@ -55,12 +55,15 @@ async function main()
         // Insert a document
         
     
-        const result = await drivercollection.insertMany(driver);
-        console.log(`New driver created with result: ${result}`);
+        //const result = await drivercollection.insertMany(driver);
+        //console.log(`New driver created with result: ${result}`);
         
-        const updateResult = await db.collection('driver').updateOne( { name: "John Doe" },
-        { $inc: { rating: -1}});
+        const updateResult = await db.collection('driver').updateOne( { name: "YIK BOON" },
+        { $inc: { rating: 0.1}});
         console.log(`Driver updated with result: ${updateResult}`);
+
+        //const deleteResult = await db.collection('driver').deleteOne({isAvailable: "true" });
+        //console.log(`Driver deleted with result: ${deleteResult}`);
 
         // const availableDrivers = await db.collection('driver').find({
         //      isAvailable: true, rating: { $gte: 4.5} 
