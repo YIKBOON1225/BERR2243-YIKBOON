04b05db9e9f878644a2e45237322fe17e5db0eb6
LIM YIK BOON <b122420030@student.utem.edu.my>
2025-10-30 03:21:10 +0000

    week3 exercise (step 1:setup project)

--- index.js
+++ index.js
@@ -1,84 +1,30 @@
+const express = require('express');
 const { MongoClient } = require('mongodb');
+const port = 3000
+const app = express();
 
-const driver = 
-[
-    {
-        name: "John Doe",
-        vehicleType: "Sedan",
-        isAvailable: true,
-        rating: 4.8
-    },
-    {
-        name: "Alice Smith",
-        vehicleType: "SUV",
-        isAvailable: false,
-        rating: 4.5
-    }
-];
+app.use(express.json());
+let db;
 
-// show the data in the console
-console.log(driver);
-
-// TODO: show the all the drivers name in the console
-console.log("Driver names:");
-driver.forEach(driver => console.log(driver.name));
-
-// TODO: add additional driver to the drivers array
-driver.push({
-  name: "YIK BOON",
-  vehicleType: "SUV",
-  isAvailable: true,
-  rating: 4.7
-});
-
-// show the data in the console
-console.log(driver);
-
-async function main()
+async function connectToMongoDB() 
 {
-    //show time operation
-    //const start = Date.now();
-
-    const uri = "mongodb://127.0.0.1:27017";
+    const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
+
     try 
     {
         await client.connect();
-
-        //show time operation
-        //const duration  = Date.now() - start;
-        //console.log(`Connected to MongoDB! (${duration} ms)`);
-
-
-        const db = client.db("testDB");
-        const drivercollection = db.collection("driver");
-        // Insert a document
-        
-    
-        //const result = await drivercollection.insertMany(driver);
-        //console.log(`New driver created with result: ${result}`);
-        
-        //const updateResult = await db.collection('driver').updateMany({vehicleType: "SUV"},{ $inc: { rating: 0.1}});
-        //console.log(`Driver updated with result: ${updateResult}`);
-
-        const deleteResult = await db.collection('driver').deleteMany({isAvailable:{$eq: true }});
-        console.log(`Driver deleted with result: ${deleteResult}`);
-
-        // const availableDrivers = await db.collection('driver').find({
-        //      isAvailable: true, rating: { $gte: 4.5} 
-        //     }).toArray();
-        // console.log("Available drivers:", availableDrivers);
-
-    }
-      
+        console.log("Connected to MongoDB!");
+        db = client.db("testDB");
+    } 
     catch (err) 
     {
-    console.error("Error:", err);
-    } 
-
-    finally 
-    {
-        await client.close();
+        console.error("Error:", err);
     }
 }
-main();
+
+connectToMongoDB();
+app.listen(port, () => 
+{
+    console.log(`Server running on port ${port}`);
+});
